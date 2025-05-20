492a20afee13567447af06aeade1c8cd27a0691e
Raúl Gavilan <rgavilan@elpuig.xeill.net>
2025-05-20 17:07:08 +0200

    v3 index.php

--- var/www/html/PORTserver/index.php
+++ var/www/html/PORTserver/index.php
@@ -29,7 +29,7 @@ foreach ($files as $file) {
 
 }
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -43,7 +43,7 @@ foreach ($files as $file) {
             top: 10px;
             left: 10px;
             padding: 5px 10px;
-            font-size: 28px;
+            font-size: 18px;
             background-color: #28a745;
             color: white;
             border: none;
@@ -60,14 +60,14 @@ foreach ($files as $file) {
         body {
             background: url('https://i.redd.it/zajy8wh63xl51.gif') no-repeat center center fixed;
             background-size: cover;
-            font-family: 'San Francisco', cursive;
+            font-family: 'Arial';
             color: #0f0;
             margin: 0;
             padding: 0;
         }
 
         .overlay {
-            background-color: rgba(0, 0, 0, 0.85);
+            background-color: rgba(0, 0, 0, 0.651);
             padding: 40px 20px;
             min-height: 100vh;
         }
@@ -76,7 +76,7 @@ foreach ($files as $file) {
             text-align: center;
             font-size: 20px;
             color: #0f0;
-            text-shadow: 0 0 10px #0f0;
+            text-shadow: 0 0 10px rgba(0, 255, 0, 0.61);
             margin-bottom: 30px;
         }
 
@@ -84,8 +84,8 @@ foreach ($files as $file) {
             width: 95%;
             margin: auto;
             border-collapse: collapse;
-            box-shadow: 0 0 20px #0f0;
-            border-radius: 12px;
+            box-shadow: 0 0 20px rgba(0, 255, 0, 0.411);
+            border-radius: 10px;
             overflow: hidden;
         }
 
@@ -96,7 +96,7 @@ foreach ($files as $file) {
         }
 
         th {
-            background-color: #000;
+            background-color: #0000009a;
             color: #0f0;
             font-size: 14px;
         }
@@ -107,12 +107,6 @@ foreach ($files as $file) {
             font-size: 14px;
         }
 
-        tr:hover td {
-            background-color: #222;
-            color: #0f0;
-            transition: 0.3s;
-        }
-
         .no-servers {
             color: #f00;
             font-size: 14px;
@@ -120,11 +114,11 @@ foreach ($files as $file) {
         }
 
         .power-btn {
-            font-family: 'Press Start 2P', cursive;
+            font-family: 'Arial', cursive;
             padding: 10px 14px;
             margin: 10px;
             font-size: 12px;
-            border: 2px solid #0f0;
+            border: 2px solid rgba(0, 255, 0, 0.534);
             border-radius: 10px;
             cursor: pointer;
             transition: 0.3s;
@@ -137,128 +131,126 @@ foreach ($files as $file) {
             float: right;
         }
 
-        .start-btn:hover {
-            background-color: #004d00;
-            box-shadow: 0 0 10px #0f0;
-        }
-
-        .stop-btn {
-            background-color: #2f0000;
-            color: #f00;
-            border-color: #f00;
-            box-shadow: 0 0 5px #f00;
-            float: left;
-        }
-
-        .stop-btn:hover {
-            background-color: #5c0000;
-            box-shadow: 0 0 10px #f00;
-        }
-
-        @media (max-width: 768px) {
-            table, th, td {
-                font-size: 8px;
-            }
-        }
-
-        .btn-row {
-            background-color: #000;
-            padding: 5px 0;
-            display: flex;
-            justify-content: space-between;
-            border-bottom: 1px solid #0f0;
-        }
-
-        .btn-row form {
-            margin: 0 10px;
-        }
-
-	.delete-btn{
-	  background-color: #2f0000;
-	  color: #3944e3;
-	  border-color: #faf20a;
-	  border-bottom: 2px solid #faf20a;
-	  box-shadow: 0 0 5px #faf20a;
-	}
-
-	a.power-btn[style*="opacity:0.5"] {
-    	 cursor: not-allowed;
-    	 pointer-events: none !important;
-	}
-
-    </style>
-</head>
-
-<script>
-function handleAction(link) {
-    link.style.pointerEvents = 'none';
-    link.style.opacity = '0.5';
-    link.textContent = 'Procesando...';
-    return true;
+.start-btn:hover {
+    background-color: #004d00;
+    box-shadow: 0 0 10px rgba(0, 255, 0, 0.26);
 }
-</script>
 
+.stop-btn {
+    background-color: #2f0000;
+    color: #f00;
+    border-color: #f00;
+    box-shadow: 0 0 5px #f00;
+    float: left;
+}
+
+.stop-btn:hover {
+    background-color: #5c0000;
+    box-shadow: 0 0 10px #f00;
+}
+
+.estado-btn {
+    background-color: #3a2213;
+    color: rgb(255, 136, 0);
+    border-color: rgb(255, 136, 0);
+    box-shadow: 0 0 5px rgb(230, 147, 38);
+    float: left;
+}
+
+.estado-btn:hover {
+    background-color: #914d0eb6;
+    box-shadow: 0 0 10px rgba(255, 145, 0, 0.466);
+}
+
+@media (max-width: 768px) {
+    table, th, td {
+        font-size: 8px;
+    }
+}
+
+.btn-row {
+    display: flex;
+}
+
+.btn-row form {
+    margin: 0 10px;
+}
+
+.delete-btn{
+background-color: #0f0e2b;
+color: #928fec;
+border-color: #3733bd;
+border-bottom: 2px solid #3733bd;
+box-shadow: 0 0 5px #5a58d1
+}
+
+.delete-btn:hover {
+    background-color: #201e53;
+    box-shadow: 0 0 10px #928fec;
+}
+</style>
+</head>
 <body>
 
-    <!-- Botón de navegación -->
-    <button class="boton-navegacion" onclick="location.href='/navegacion.html'">Tornar a la pàgina principal</button>
+<!-- Botón de navegación -->
+<button class="boton-navegacion" onclick="location.href='/navegacion.html'">Tornar a la pàgina principal</button>
 
-    <div class="overlay">
-        <h1> Contenidors de Terraria actius </h1>
+<div class="overlay">
+<h1> Contenidors de Terraria actius </h1>
 
-        <table>
-            <thead>
+<table>
+    <thead>
+        <tr>
+            <th>Contenidor</th>
+            <th>Món</th>
+            <th>Llavor</th>
+            <th>Jugadors</th>
+            <th>Dificultat</th>
+            <th>Port</th>
+            <th>IP</th>
+            <th>Estat</th>
+            <th>Data</th>
+            <th>Botones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (count($servers) > 0): ?>
+            <?php foreach ($servers as $s): ?>
                 <tr>
-                    <th>Contenidor</th>
-                    <th>Món</th>
-                    <th>Llavor</th>
-                    <th>Jugadors</th>
-                    <th>Dificultat</th>
-                    <th>Port</th>
-                    <th>IP</th>
-                    <th>Estat</th>
-                    <th>Data</th>
+                    <td><?= htmlspecialchars($s['nombre']) ?></td>
+                    <td><?= htmlspecialchars($s['mundo']) ?></td>
+                    <td><?= htmlspecialchars($s['semilla']) ?></td>
+                    <td><?= htmlspecialchars($s['jugadores']) ?></td>
+                    <td>
+                        <?php
+                            switch ($s['dificultad']) {
+                                case '0': echo 'Normal'; break;
+                                case '1': echo 'Experto'; break;
+                                case '2': echo 'Maestro'; break;
+                                default: echo 'Desconocida';
+                            }
+                        ?>
+                    </td>
+                    <td><?= htmlspecialchars($s['puerto']) ?></td>
+                    <td><?= htmlspecialchars($s['ip']) ?></td>
+                    <td><?= htmlspecialchars($s['estado']) ?></td>
+                    <td><?= htmlspecialchars($s['fecha']) ?></td>
+                    <td >
+                        <div class="btn-row">
+                            <a href="apagar.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn stop-btn">APAGAR</a>
+                            <a href="borrar.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn delete-btn">BORRAR</a>
+                            <a href="start.php?name=<?php echo htmlspecialchars($s['nombre']) ?>&mundo=<?php echo htmlspecialchars($s['mundo']) ?>" class="power-btn start-btn">ENCENDER</a>
+                            <a href="estatus.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn estado-btn" onclick="return handleAction(this)">STATUS</a>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php if (count($servers) > 0): ?>
-                    <?php foreach ($servers as $s): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($s['nombre']) ?></td>
-                            <td><?= htmlspecialchars($s['mundo']) ?></td>
-                            <td><?= htmlspecialchars($s['semilla']) ?></td>
-                            <td><?= htmlspecialchars($s['jugadores']) ?></td>
-                            <td>
-                                <?php
-                                    switch ($s['dificultad']) {
-                                        case '0': echo 'Normal'; break;
-                                        case '1': echo 'Experto'; break;
-                                        case '2': echo 'Maestro'; break;
-                                        default: echo 'Desconocida';
-                                    }
-                                ?>
-                            </td>
-                            <td><?= htmlspecialchars($s['puerto']) ?></td>
-                            <td><?= htmlspecialchars($s['ip']) ?></td>
-                            <td><?= htmlspecialchars($s['estado']) ?></td>
-                            <td><?= htmlspecialchars($s['fecha']) ?></td>
-                            <td colspan="9">
-                                <div class="btn-row">
-					<a href="apagar.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn stop-btn" onclick="return handleAction(this)">APAGAR</a>
-                                        <a href="borrar.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn delete-btn" onclick="return handleAction(this)">BORRAR</a>
-                                        <a href="start.php?name=<?php echo htmlspecialchars($s['nombre']) ?>&mundo=<?php echo htmlspecialchars($s['mundo']) ?>" class="power-btn start-btn" onclick="return handleAction(this)">ENCENDER</a>
-					<a href="estatus.php?name=<?php echo htmlspecialchars($s['nombre']) ?>" class="power-btn estado-btn" onclick="return handleAction(this)">STATUS</a>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <tr><td colspan="9" class="no-servers">No hay servidores activos </td></tr>
-                <?php endif; ?>
-            </tbody>
-        </table>
-    </div>
-
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr><td colspan="10" class="no-servers">No hay servidores activos </td></tr>
+        <?php endif; ?>
+    </tbody>
+</table>
+</div>
 </body>
 </html>
